16131dd5851ed6929d32d2dbc277bf7d8e2d7203
Hiago <hiagusr@gmail.com>
2025-09-19 14:41:09 -0300

    Adicionando Botão que muda de tela

--- app/src/main/java/com/example/eeg_v0/MainActivity.kt
+++ app/src/main/java/com/example/eeg_v0/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.eeg_v0
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.eeg_v0.databinding.ActivityMainBinding // Import para a classe de binding gerada
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.button.setOnClickListener {
+            var intent = Intent(this, view::class.java)
+            startActivity(intent)
         }
     }
 }
